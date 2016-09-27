28d379c33ba4dd495c2cf0b50db71cf736e2d822
pugovka <ms.n.yunak@gmail.com>
2016-09-28 09:52:30 +1000

    Init project

---
+++ TaskTracker.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskTracker.WebAPI.WebApiApplication" Language="C#" %>
---
+++ TaskTracker.WebAPI/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace TaskTracker.WebAPI.Models
+{
+    public class Task
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class TaskDBContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
